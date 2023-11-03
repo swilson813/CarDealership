48e77789831aaf8a932c6f1074ea35ac88014f9b
swilson813 <swilson@appdev.yearup.org>
2023-11-03 13:39:57 -0400

    updates

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,84 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Dealership {
+   private String name;
+   private String address;
+   private String phone;
+
+    //Instantiate object
+    private ArrayList<Vehicle>inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<Vehicle>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealership{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+    public ArrayList<Vehicle> getVehicleByPrice(double min, double max) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByMakeModel(String make, String model){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByYear(String min, String max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByMileage(double min, double max) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByType(String type) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return this.inventory;
+    }
+    public void addVehicle(Vehicle vehicle){
+        this.inventory.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle){
+
+    }
 }
+
--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
+import java.io.*;
+
 public class DealershipFileManager {
+
+    public Dealership getDealership() {
+        //Return dealership info "Car World|123 Main St|555-123-4567"
+        Dealership dealership1 = new Dealership(" ", " ", " ");
+        String line;
+
+        int lineNumber = 1;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("inventory.csv"));
+
+
+            while ((line = br.readLine()) != null) {
+                String[] pipes = line.split("\\|");
+                if (lineNumber == 1) {
+                    String name = pipes[0];
+                    dealership1.setName(name);
+                    String address = pipes[1];
+                    String phoneNumber = pipes[2];
+                } else {
+                    int vin = Integer.parseInt(pipes[0]);
+                    int year = Integer.parseInt(pipes[1]);
+                    String make = pipes[2];
+                    String model = pipes[3];
+                    String vehicleTypes = pipes[4];
+                    String color = pipes[5];
+                    int odometer = Integer.parseInt(pipes[6]);
+                    double price = Double.parseDouble(pipes[7]);
+                    Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleTypes, color, odometer, price);
+                    dealership1.addVehicle(vehicle);
+                }
+
+                lineNumber++;
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dealership1;
+    }
+    public void saveDealership(Dealership dealership){
+
+
+    }
 }
